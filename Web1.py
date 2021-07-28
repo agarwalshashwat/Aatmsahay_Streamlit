0f8264e2c5651f33dc21db37d5e6df9bf2e2c9a7
Shashwat Agarwal <66725004+agarwalshashwat@users.noreply.github.com>
2021-07-28 12:12:50 +0530

    Update Web1.py

--- Web1.py
+++ Web1.py
@@ -68,8 +68,8 @@ def main():
         c = result[2]
     st.success('The disease you might be having according to the three models i.e; by  random forest classifier is {}, by naive bayes classifier is {} and at last by k-nearest neighbour is {}.'.format(a,b,c))
     if st.button("About"):
-        st.text("This is a teamwork of Prabhat, Shagun, Shruti and Shashwat")
-        st.text("Built with Streamlit")
+        st.text("This is just a prototype, when in need please consult a doctor.")
+        st.text("Built by Shashwat Agarwal")
     
 if __name__ == "__main__":
     main()
