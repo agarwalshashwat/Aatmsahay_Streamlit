bba3e9fae46f6a6b99dd0b128f0f66c32478374f
Shashwat Agarwal <66725004+agarwalshashwat@users.noreply.github.com>
2021-07-28 13:41:21 +0530

    Update Web1.py

--- Web1.py
+++ Web1.py
@@ -54,7 +54,7 @@ def main():
     </div>
     '''
     st.markdown(html_temp,unsafe_allow_html=True)
-    Symptom_1 = str(st.text_input('Primary Symptom','Type Here'))
+    Symptom_1 = st.selectbox('Choose your primary symptom from the following list',('Vomiting','Shivering'))
     Symptom_2 = str(st.text_input('Secondary Symptom', 'Type Here'))
     Symptom_3 = str(st.text_input('Tertiary Symptom','Type Here'))
     result = ''
