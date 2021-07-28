dcc7a0222c80e61fb698593edfa1741d937f1b54
Shashwat Agarwal <66725004+agarwalshashwat@users.noreply.github.com>
2021-07-28 13:55:14 +0530

    Update Web1.py

--- Web1.py
+++ Web1.py
@@ -54,7 +54,7 @@ def main():
     </div>
     '''
     st.markdown(html_temp,unsafe_allow_html=True)
-    Symptom_1 = st.selectbox('Primary Symptom',{'Select Your Primary Symptom':0,'Vomiting':'vomiting','Shivering':'shivering'})
+    Symptom_1 = st.selectbox('Primary Symptom',{'Select your primary symptom':0,'Abdominal Pain':'abdominal_pain','Abnormal Menstruation':'abnormal_menstruation','Acidity':'acidity','Acute Liver Failure':'acute_liver_failure'})
     Symptom_2 = str(st.text_input('Secondary Symptom', 'Type Here'))
     Symptom_3 = str(st.text_input('Tertiary Symptom','Type Here'))
     result = ''
